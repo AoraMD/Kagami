023548f8725e2b3046d7d62e710d4bae75ed2854
AoraMD <develop@aoramd.moe>
2023-04-08 19:23:38 +0800

    Convert scut to tracking file instead of submodule for maintaining.

---
+++ _sass/scut/lib/scut.rb
@@ -0,0 +1,10 @@
+require 'compass'
+
+base_directory  = File.join(File.dirname(__FILE__), '..')
+stylesheets_dir = File.join(base_directory, 'dist')
+Compass::Frameworks.register('Scut', :stylesheets_directory => stylesheets_dir)
+
+module Scut
+  VERSION = "2.0.0"
+  DATE = "2022-02-01"
+end
